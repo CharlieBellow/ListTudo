676c0ffacd285a27c159c85473e663b3be8b757e
Charlie Bellow <charliebftm@gmail.com>
2022-10-19 17:45:13 -0300

    force

--- list-tudo/.storybook/main.cjs
+++ list-tudo/.storybook/main.cjs
@@ -18,7 +18,7 @@ module.exports = {
 
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/list-tudo/'
+      config.base = '../list-tudo/'
     }
 
     return config
